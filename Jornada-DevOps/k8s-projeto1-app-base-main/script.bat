12a156a03a255beb478a2e78ce11640133bb08c0
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-08 19:00:30 -0300

    script.bat criado

--- Jornada-DevOps/k8s-projeto1-app-base-main/script.bat
+++ Jornada-DevOps/k8s-projeto1-app-base-main/script.bat
@@ -1,1 +1,7 @@
-@REM 
+echo "Criando as imagens..."
+docker build -t projeto-backend:1.0 backend/.
+docker build -t projeto-backend:1.0 database/.
+
+echo "Realizando push das imagens..."
+docker build -t projeto-backend:1.0 
+docker build -t projeto- database:1.0
