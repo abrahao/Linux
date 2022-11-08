6c895224ba072bd9eb7c6e0096fcec3de2df6f99
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-08 19:06:45 -0300

    script.bat updated

--- Jornada-DevOps/k8s-projeto1-app-base-main/script.sh
+++ Jornada-DevOps/k8s-projeto1-app-base-main/script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Criando as imagens..."
 docker build -t projeto-backend:1.0 backend/.
 docker build -t projeto-backend:1.0 database/.
@@ -5,3 +7,9 @@ docker build -t projeto-backend:1.0 database/.
 echo "Realizando push das imagens..."
 docker build -t projeto-backend:1.0 
 docker build -t projeto- database:1.0
+
+echo "Criando servicos no cluster kubernetes...""
+kubctl apply -f ./service.yml
+
+echo "Criando os deployments"
+kubctl apply -f ./deployments.yml
